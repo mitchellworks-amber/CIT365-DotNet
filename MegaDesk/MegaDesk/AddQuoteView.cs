d8a1e6a2b84ea6baa6630b0ac6e7b48bbf371f34
mitchellworks-amber <mitchellworks@gmail.com>
2019-01-15 14:39:02 -0700

    Starting MegaDesk, views somewhat working.

---
+++ MegaDesk/MegaDesk/AddQuoteView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk
+{
+    public partial class AddQuoteView : Form
+    {
+        public AddQuoteView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayQuoteView view = new DisplayQuoteView();
+            view.ShowDialog();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ MegaDesk/MegaDesk/Desk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk
+{
+    //set up the possible wood types
+    enum WoodMaterial
+    {
+        oak,
+        laminate,
+        pine,
+        rosewood,
+        veneer,
+    }
+
+    public class Desk
+    {
+        int width;
+        int depth;
+        int drawers;
+        string material;
+        bool oversized = false;
+
+        static bool ValidateInputs()
+        {
+            return true;
+        }
+
+    }
+}
