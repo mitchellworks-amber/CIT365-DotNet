629eaccda919e4f4e4dbd6af56fd283778fb7377
mitchellworks-amber <mitchellworks@gmail.com>
2019-03-15 18:52:17 -0600

    LDS Movies Web App (MVC) done.

---
+++ LDSMovies/LDSMovies/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDSMovies.Controllers
+{
+    public class MainController : Controller
+    {
+        // 
+        // GET: /Main/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /Main/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ LDSMoviesWeb/LDSMoviesWeb/Data/LDSMoviesWebContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LDSMoviesWeb.Models
+{
+    public class LDSMoviesWebContext : DbContext
+    {
+        public LDSMoviesWebContext (DbContextOptions<LDSMoviesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LDSMoviesWeb.Models.Movie> Movie { get; set; }
+    }
+}
